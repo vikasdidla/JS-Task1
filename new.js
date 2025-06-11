b305148f05fce64499ef0ac1295b19cbc15f5197
vikasdidla <vikasdidla225@gmail.com>
2025-06-11 22:55:33 +0530

    js without html

---
+++ new.js
@@ -0,0 +1,51 @@
+let details=document.createElement("h1")
+details.innerHTML=`<center><u><i><b>**MY -DETAILS**</b></i></u></center>`
+document.body.append(details)
+
+let h1=document.createElement("h1")
+h1.innerHTML=`<i># Name:- Vikas Didla</i>`
+document.body.append(h1)
+
+let p=document.createElement("h2")
+p.innerHTML="# Born on :- 2000/08/15"
+document.body.append(p)
+
+let p1=document.createElement("h3")
+p1.innerHTML=`# <i>Age:- 23</i>`
+document.body.append(p1)
+
+let h2=document.createElement("h4")
+h2.innerHTML="# Status:- Studying"
+document.body.append(h2)
+
+let p2=document.createElement("h5")
+p2.innerHTML=`# <i>Village:- Sankaraguptam</i>`
+document.body.append(p2)
+
+let ph=document.createElement("p")
+ph.innerHTML=` <hr>`
+document.body.append(ph)
+
+let p3=document.createElement("h5")
+p3.innerHTML="# Qualification:- B-Tech"
+document.body.append(p3)
+
+let p4=document.createElement("h4")
+p4.innerHTML=`# <i>Learning:- Python Full Stack</i>`
+document.body.append(p4)
+
+let p5=document.createElement("h3")
+p5.innerHTML="# Institute:- 10000 Coders"
+document.body.append(p5)
+
+let p6=document.createElement("h2")
+p6.innerHTML=`# <i>Location:- Hyderbad</i>`
+document.body.append(p6)
+
+let p7=document.createElement("h1")
+p7.innerText="# Suggestion:-Don't Judge a Book By It's Cover"
+document.body.append(p7)
+
+let p8=document.createElement("h1")
+p8.innerHTML=`<center><i><u><b>*******&nbsp&nbspTHE END&nbsp&nbsp*******</b></u></i></center>`
+document.body.append(p8)
